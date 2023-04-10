31cdb6dd7aeac42816c1b151bcbff647beff6572
Tharicq1409 <sheiktharicq14@gmail.com>
2023-04-10 15:09:50 +0530

    form tutorial completed

--- forms_trail/formapp/views.py
+++ forms_trail/formapp/views.py
@@ -1,3 +1,7 @@
 from django.shortcuts import render
-
+from .import forms
 # Create your views here.
+def empformview(request):
+    form = forms.Employee_info()
+    empinfo = {'empform':form}
+    return render(request,'formapp/index.html',context=empinfo)
