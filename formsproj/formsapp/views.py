f6dc9e2084a57e168a9360add055369a9e061252
Tharicq1409 <sheiktharicq14@gmail.com>
2023-04-11 06:20:20 +0530

    added comments

--- formsproj/formsapp/views.py
+++ formsproj/formsapp/views.py
@@ -2,11 +2,11 @@ from django.shortcuts import render
 from formsapp.models import Employee
 # Create your views here.
 def index(request):
-    if request.method == "POST":
-        if request.POST.get('firstname') and request.POST.get('lastname'):
-            emp_var = Employee()
-            emp_var.first_name = request.POST.get('firstname')
-            emp_var.last_name = request.POST.get('lastname')
-            emp_var.save()
-        return render(request,'success.html',{})
-    return render(request,'index.html',{})
+    if request.method == "POST":   #if it is a POST Method then ...
+        if request.POST.get('firstname') and request.POST.get('lastname'):  #if the values are present then it gets user input
+            emp_var = Employee() #creating object for Employee model
+            emp_var.first_name = request.POST.get('firstname')  #----|-->sending user input to Employee Model
+            emp_var.last_name = request.POST.get('lastname')  #-----|
+            emp_var.save()   #saving datas to model
+        return render(request,'success.html',{})      #Display success message Page
+    return render(request,'index.html',{})            #user Input Page
